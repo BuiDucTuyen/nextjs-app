b15640ebef4ed2e0bbcc5f4a50d9579af7f8d0fd
noblu <nguyenkiencuong09c4a@gmail.com>
2024-05-20 11:54:47 +0700

    update-button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,9 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
             "bgColor": 'linear-gradient(90deg, rgb(249, 237, 234) 3%, rgb(255, 238, 238) 66%, rgb(248, 242, 211) 96%), url(https://zerius.io/img/bg.png)',
       },
+      backgroundColor:{
+        "bg-button": "#7a6eaa"
+      }
     },
   },
   plugins: [],
